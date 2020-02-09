ccbb63d5da3309cec5e80ad3ed07774dbad53378
tongmaster <basfers@gmail.com>
2020-02-09 15:21:29 +0700

    seperate form and list

--- pages/todolist.js
+++ pages/todolist.js
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 
-function Index() {
+function TodoForm() {
   const [input, setInput] = useState('')
   const changeInput = event => setInput(event.target.value)
-
-  const [todos, setTodos] = useState([])
   return (
     <>
       <input type="text" onChange={changeInput} />
       <button onClick={() => setTodos([{ id: +new Date(), input }, ...todos])}>
         add
       </button>
+    </>
+  )
+}
+function TodoList() {
+  const [todos, setTodos] = useState([])
+  return (
+    <>
       <ul>
         {todos.map(todo => (
           <li key={todo.id}> {todo.input}</li>
@@ -18,5 +23,8 @@ function Index() {
       </ul>
     </>
   )
+}
+function Index() {
+  return <></>
 }
 export default Index
