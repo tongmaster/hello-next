0106375e7f6c7bb588553534dc9d993bfdf091af
tongmaster <basfers@gmail.com>
2020-02-09 14:58:50 +0700

    add list

--- pages/todolist.js
+++ pages/todolist.js
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 
 function Index() {
-  const [input, setInput] = useState([])
+  const [input, setInput] = useState('')
+  const changeInput = event => setInput(event.target.value)
+
+  const [todos, setTodos] = useState([])
   return (
     <>
-      <input type="text" onChange={event => setInput(event.target.value)} />
-      <button onClick={() => setInput([...input, { id: +new date(), input }])}>
+      <input type="text" onChange={changeInput} />
+      <button onClick={() => setTodos([{ id: +new Date(), input }, ...todos])}>
         add
       </button>
       <ul>
-        {input.map(inputa => (
-          <li key={inputa.id}> {inputa.input}</li>
+        {todos.map(todo => (
+          <li key={todo.id}> {todo.input}</li>
         ))}
       </ul>
     </>
